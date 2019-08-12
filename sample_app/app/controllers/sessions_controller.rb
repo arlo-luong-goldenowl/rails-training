441a1e4f39f15c4dbf46f5317f674adf67e14c08
arlo-luong-goldenowl <arlo-luong-goldenowl@gmail.com>
2019-08-12 15:57:30 +0700

    add session controller, session helper, login action, store user in session

---
+++ sample_app/app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    email = params[:session][:email].downcase
+    password = params[:session][:password]
+    user = User.find_by(email: email)
+    if(user && user.authenticate(password))
+      log_in(user)
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid password'
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+
+end
--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
   def new
+    if current_user
+      flash[:warning] = "Already logged"
+      redirect_to current_user
+    else
+      @user = User.new
+    end
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)    # Not the final implementation!
+    @user.password_digest = User.digest(user_params[:password])
+    if @user.save
+      login(@user)
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
