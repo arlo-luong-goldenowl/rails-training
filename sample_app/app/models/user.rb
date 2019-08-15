db7511106ede1d28a4a3218ec4dc12a4312dc67c
arlo-luong-goldenowl <arlo-luong-goldenowl@gmail.com>
2019-08-15 16:14:34 +0700

    password reset feature, add password reset controller, add new fields for user model, reset_digest, time when reset

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
 
@@ -44,7 +44,6 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 
-
   # Activates an account.
   def activate
     update_columns(activated: true, activated_at: Time.zone.now)
@@ -53,6 +52,20 @@ class User < ApplicationRecord
   # Sends activation email.
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
+  end
+
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
 
   private
