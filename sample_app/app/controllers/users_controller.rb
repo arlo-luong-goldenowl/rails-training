07cf06451cfa08191f7d9b33ad77c64629c7c72b
arlo-luong-goldenowl <arlo-luong-goldenowl@gmail.com>
2019-08-16 10:51:05 +0700

    add micropost, resize image whe add post, validate image size before add

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
   # user profile infomation
   def show
     @user = User.find(params[:id])
-    redirect_to root_url
+    @microposts = @user.microposts.paginate(page: params[:page], per_page: 7)
   end
 
   # user setting
@@ -76,15 +76,6 @@ class UsersController < ApplicationController
     # return hash from request body
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
-
-     # Confirms a logged-in user.
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
     end
 
     # correct user
